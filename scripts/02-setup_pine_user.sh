416fb25bbffc6c7580a4365681d7357dc8a58e6c
Åke Forslund <ake.forslund@gmail.com>
2019-06-05 08:17:13 +0200

    Add kwallet related files

--- scripts/02-setup_pine_user.sh
+++ scripts/02-setup_pine_user.sh
@@ -18,6 +18,11 @@ mkdir -p /home/$USER/Pictures
 mkdir -p /home/$USER/Videos
 mkdir -p /home/$USER/Downloads
 mkdir -p /home/$USER/Documents
+
+# Install default kwallet files
+mkdir -p /home/$USER/.local/share
+cp -r /etc/skel/.local/kwalletd /home/$USER/.local/share
+
 chown -R $UGID:$UGID /home/$USER
 
 usermod -a -G ${DEFGROUPS} ${USER}
