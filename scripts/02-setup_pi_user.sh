254bd886fc79b85e5e5308ac2406752a5616455a
Bhushan Shah <bhush94@gmail.com>
2019-10-30 16:12:28 +0530

    Add scripts to setup user account and locale

---
+++ scripts/02-setup_pi_user.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -x
+
+USER=pi
+GECOS=pi
+UGID=32011
+PASSWORD=mycroft
+
+DEFGROUPS="tty,sudo,adm,dialout,cdrom,plugdev,audio,dip,video,i2c"
+
+echo "I: creating default user $USER"
+adduser --gecos $GECOS --disabled-login $USER --uid $UGID
+
+echo "I: set user $USER password to $PASSWORD"
+echo "$USER:$PASSWORD" | /usr/sbin/chpasswd
+
+mkdir -p /home/$USER/Music
+mkdir -p /home/$USER/Pictures
+mkdir -p /home/$USER/Videos
+mkdir -p /home/$USER/Downloads
+mkdir -p /home/$USER/Documents
+
+# Install default kwallet files
+mkdir -p /home/$USER/.local/share
+cp -r /etc/skel/.local/kwalletd /home/$USER/.local/share
+
+chown -R $UGID:$UGID /home/$USER
+
+usermod -a -G ${DEFGROUPS} ${USER}
