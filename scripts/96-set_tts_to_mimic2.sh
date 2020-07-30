12725ca5403890ddf60cdac5db6d5ae955367cd8
Åke Forslund <ake.forslund@gmail.com>
2020-07-30 22:55:38 +0200

    Download and install Mimic-2 Cache through recipe

--- scripts/96-set_tts_to_mimic2.sh
+++ scripts/96-set_tts_to_mimic2.sh
@@ -4,17 +4,6 @@
 UGID=32011
 USER=mycroft
 
-# TODO - host this properly
-cache_zip_url='https://frozenmazegames.se/Mimic2-local-cache.zip'
-
-dir='/opt/mycroft/preloaded_cache/Mimic2'
-mkdir -p $dir
-pushd $dir
-wget $cache_zip_url
-unzip Mimic2-local-cache.zip
-rm Mimic2-local-cache.zip
-popd
-
 MYCROFT_CONF_PATH="/home/$USER/.mycroft/mycroft.conf"
 (cat $MYCROFT_CONF_PATH || echo "{}") | jq '. + {"tts": {"module": "mimic2"}}' > /tmp/mycroft.conf
 cp /tmp/mycroft.conf $MYCROFT_CONF_PATH
