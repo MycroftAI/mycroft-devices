bb96d4257f422cce537501c1636ab7b2d5e4da99
Åke Forslund <ake.forslund@gmail.com>
2020-11-11 09:37:49 +0100

    Get architecture dependent tarball

--- scripts/92-clone-mycroft.sh
+++ scripts/92-clone-mycroft.sh
@@ -2,8 +2,8 @@ UGID=32011
 USER=mycroft
 
 # Unpack mycroft-core-setup-aarch64.tar.gz to user home
-tar -xzf /var/tmp/mycroft-core-setup-aarch64.tar.gz -C /home/$USER/
-rm /var/tmp/mycroft-core-setup-aarch64.tar.gz
+tar -xzf /var/tmp/mycroft-core-setup.tar.gz -C /home/$USER/
+rm /var/tmp/mycroft-core-setup.tar.gz
 
 mkdir -p /opt/mycroft/skills
 mkdir -p /var/log/mycroft
